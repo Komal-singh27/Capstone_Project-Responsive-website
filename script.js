d035122212f5a26a9d31c959a12a8275c6a2b6c7
Komal-singh27 <komalrai27@gmail.com>
2025-02-06 00:24:34 -0600

    Updated logic for menu close button

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
     //Toggle mobile menu visibility
-    console.log("test");
-    document.body.classList.toggle( "show-mobile-menu",true);
+    document.body.classList.toggle( "show-mobile-menu");
 
-})
+});
+
+menuCloseButton.addEventListener("click", () => {
+    menuOpenButton.click();
+
+});
 
