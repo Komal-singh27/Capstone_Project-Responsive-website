4b811d85ac44bf04c17fe73cd00c9e9a3996f4c8
Komal-singh27 <komalrai27@gmail.com>
2025-02-06 19:09:14 -0600

    Menu section

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle( "show-mobile-menu");
 
 });
-
+//close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => {
     menuOpenButton.click();
 
